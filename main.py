bc8cc4243dd12d30bdf605dab3c3d39c1dab0ce1
CD6RJK21 <sanyo3102@gmail.com>
2021-07-15 15:47:03 +0300

    finally...........................

--- main.py
+++ main.py
@@ -39,15 +39,9 @@ def get_token_price(trade):
     return price
 
 
-# sold = cryptocompare.get_historical_price(trade[n]['tokenSymbol'].upper(),
-#                                             'USD', trade[n]['timeStamp'])[trade[n]['tokenSymbol'].upper()]['USD']
-#
-# earned = cryptocompare.get_historical_price(trade[n % 2]['tokenSymbol'].upper(),
-#                                             'USD', trade[n % 2]['timeStamp'])[trade[n % 2]['tokenSymbol'].upper()]['USD']
-
-
 async def main():
-    address = input()
+    address = input()  # 0x1e2c0f0cc139a3781f866cb5b383c57ed620f7ca
+    global client
     async with BscScan(API_KEY) as client:
         curr_price = await client.get_bnb_last_price()
         curr_price = float(curr_price['ethusd'])
@@ -64,38 +58,36 @@ async def main():
             )
         bought_tokens = dict()
         percents = []
-        txs_in_trade = 0
-        value_plus = 0
-        n = 0
-        for trade in address_trades:  # maybe use % of sold to all amount of bought
-            n += 1
+        for trade in address_trades:
             try:
                 hash_value = await client.get_internal_txs_by_txhash(txhash=trade['hash'])
             except AssertionError as ae:
                 continue
-            if n < (len(address_trades) - 1) and trade['nonce'] == address_trades[n]['nonce']:
-                txs_in_trade = 2
-            txs_in_trade -= 1
             hash_value = hash_value[0]
-            bnb_per_coin = int(hash_value.get('value')) / (int(trade['value']) + value_plus)
-            if trade.get('to').lower() == address.lower():
-                bought_tokens[trade.get('tokenSymbol')] = bnb_per_coin
-            elif txs_in_trade <= 0:
+
+            if trade.get('to').lower() == address.lower():  # buying
                 if trade.get('tokenSymbol') in bought_tokens:
-                    if bought_tokens[trade.get('tokenSymbol')]:
-                        value_plus = 0
-                        # k = int(trade.get('value')) / bought_tokens[trade.get('tokenSymbol')][1]
-                        bought = bought_tokens[trade.get('tokenSymbol')]
-                        sold = bnb_per_coin
-                        if bought != 0:
-                            profit = (sold - bought) / bought * 100
-                            if profit != 0.0:
-                                if len(percents) > 0 and (percents[-1] > profit + 0.5 or percents[-1] < profit - 0.5):
-                                    percents.append(profit)
-                                elif len(percents) == 0:
-                                    percents.append(profit)
-            else:
-                value_plus += int(trade['value'])
+                    bought_tokens[trade.get('tokenSymbol')][0] += int(hash_value.get('value')) / 1000000000000000000
+                    bought_tokens[trade.get('tokenSymbol')][1] += int(trade.get('value'))
+                else:
+                    bought_tokens[trade.get('tokenSymbol')] = [int(hash_value.get('value')) / 1000000000000000000,
+                                                               int(trade.get('value')), 0, 0]
+            else:  # selling
+                if trade.get('tokenSymbol') in bought_tokens:
+                    bought_tokens[trade.get('tokenSymbol')][2] += int(hash_value.get('value')) / 1000000000000000000
+                    bought_tokens[trade.get('tokenSymbol')][3] += int(trade.get('value'))
+                else:
+                    bought_tokens[trade.get('tokenSymbol')] = [0, 0, int(hash_value.get('value')) / 1000000000000000000,
+                                                               int(trade.get('value'))]
+            time.sleep(0.2)
+        for key in bought_tokens.keys():
+            bought = bought_tokens[key][0]
+            sold = bought_tokens[key][2]
+            if bought != 0:
+                roi = sold / bought * 100
+                if roi != 100 and roi != 0:
+                    percents.append(roi)
+
         if len(percents) > 0:
             print(percents)
             print('Average profit: ', sum(percents) / len(percents), '%', sep='')
@@ -107,5 +99,4 @@ if __name__ == "__main__":
     cg = CoinGeckoAPI()
     coins_list = cg.get_coins_list()
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    for i in range(5):
-        asyncio.run(main())
+    asyncio.run(main())
